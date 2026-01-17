bb079c7e828893d058c67d6c7abae4dd1a833ac3
Agus <agusgus612@gmail.com>
2026-01-18 01:40:56 +0700

    Upload Project

---
+++ CrudMvcBarang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudMvcBarang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
